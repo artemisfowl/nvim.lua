4dc8f95fff03a4854322504d59795ee053111bcc
oldgod <skb655952@gmail.com>
2024-11-02 20:34:52 +0530

    Updated keymapping for moving lines of code

--- lua/core/generic.lua
+++ lua/core/generic.lua
@@ -235,7 +235,7 @@ vim.cmd[[
 	xnoremap <C-n> :m'>+<CR>gv=gv
 
 	" move current line up one line
-	nnoremap <C-p>  :<C-u>m-2<CR>==
+	nnoremap <C-m>  :<C-u>m-2<CR>==
 
 	" move current line down one line
 	nnoremap <C-n> :<C-u>m+<CR>==
